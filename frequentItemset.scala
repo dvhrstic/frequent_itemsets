2d0dc85946f89fa3cd11f1967be5be6d72f700cf
Dusan <mac@n167-p77.eduroam.kth.se>
2018-11-15 15:00:56 +0100

    Singletons and doubles

--- frequentItemset.scala
+++ frequentItemset.scala
@@ -13,21 +13,37 @@ object FrequentItemSets {
 		//Define a list of all itemsets
 		var counts: Array[Int] = Array.fill(1000)(0)
 
-
 		for(basket <- Source.fromFile(fileName).getLines) {
 			for(item <- basket.split(" ")) {
 				counts(item.toInt) += 1
 			}
 		}
-		counts.foreach(println)
 
+		var item = 0;
+		var indexMap = 0;
+		var frequentItems: Map[Int, Int] = Map()
+		for(item <- counts){
+			if(item >= s ){
+				frequentItems = frequentItems + (indexMap -> item)
+				indexMap += 1;
+			}
+			item += 1;
+		}
+
+		
+		frequentItems =	aPriori(frequentItems, k, s)
+	
 	}
 
-	def aPriori(frequentItems: Set[Int], k: Int, s: Int) {
+	def aPriori(frequentItems: Map[Int, Int], k: Int, s: Int) {
 		//fetch each basket from the data file
 		val fileName: String = "dataset/testDat.dat"
-		for(line <- Source.fromFile(fileName).getLines) {
-			println(line)
+		while(k > 0) {
+			for(basket <- Source.fromFile(fileName).getLines) {
+				for(item <- basket.split(" ")) {
+				counts(item.toInt) += 1
+				}
+			}
 		}
 	}
 }
