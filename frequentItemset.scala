aed5c4bb8e17ea2db33d92c47dc70a536b83fdef
Dusan <mac@n167-p77.eduroam.kth.se>
2018-11-15 14:04:19 +0100

    Singletons initialization

--- frequentItemset.scala
+++ frequentItemset.scala
@@ -4,23 +4,21 @@ import util.control.Breaks._
 object FrequentItemSets {
 
 	def main(args:Array[String]){
-
-		val fileName: String = "dataset/testDat.dat"
 		//The size of the itemsets
 		val k = 2
 		//The support needed for frequent itemsets
 		val s = 1
-
 		//Define a list of all itemsets
-		var singletons: Set[Set[Int]]
-
-		for(i <- 1 to n) yield {
-    			r.nextInt(n)
+		var singletons: Set[Set[Int]] = Set()
+		for(i <- 0 to 999) yield {
+    			singletons = singletons + Set(i)
 		}
+		println(singletons)
 	}
 
 	def aPriori(frequentItems: Set[Int], k: Int, s: Int) {
 		//fetch each basket from the data file
+		val fileName: String = "dataset/testDat.dat"
 		for(line <- Source.fromFile(fileName).getLines) {
 			println(line)
 		}
